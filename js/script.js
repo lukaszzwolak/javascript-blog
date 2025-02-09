f8291ff37721e1f27604cd513affb04e2b173283
lukaszzwolak <lukasz.zwolak1607@gmail.com>
2025-02-09 16:38:00 +0100

    feat: Add 'active' class to clicked article

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 const titleClickHandler = function (event) {
+  event.preventDefault();
   const clickedElement = this;
   console.log('Link was clicked!');
 
-  /* remove class 'active' from all article links  */
+  /* remove class 'active' from all article links */
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for (let activeLink of activeLinks) {
@@ -11,25 +12,36 @@ const titleClickHandler = function (event) {
 
   /* add class 'active' to the clicked link */
   console.log('clickedElement:', clickedElement);
-  //console.log('clickedElement (with plus): ' + clickedElement);
   clickedElement.classList.add('active');
 
   /* remove class 'active' from all articles */
-  const activeArticles = document.querySelectorAll('.posts a.active');
+  const activeArticles = document.querySelectorAll('.posts article.active');
 
   for (let activeArticle of activeArticles) {
     activeArticle.classList.remove('active');
   }
 
   /* get 'href' attribute from the clicked link */
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log('articleSelector', articleSelector);
 
   /* find the correct article using the selector (value of 'href' attribute) */
-
+  const targetArticle = document.querySelector(articleSelector);
+  console.log('targetArticle', targetArticle);
+  
   /* add class 'active' to the correct article */
-}
+  console.log('clickedElement:', clickedElement);
+  targetArticle.classList.add('active');
+};
 
 const links = document.querySelectorAll('.titles a');
 
 for (let link of links) {
   link.addEventListener('click', titleClickHandler);
 }
+
+
+/*
+Nie wiem dlaczego cały blog po odpaleniu w wyszukiwarce przesuwa
+się na lewo... Prosiłbym o intrukcję ://// 
+*/
