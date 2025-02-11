59a44983e28e404d7f062cf30d4b6e4308957e06
lukaszzwolak <lukasz.zwolak1607@gmail.com>
2025-02-11 12:24:38 +0100

    Removed existing <li> elements and added a for...of loop to generate article links

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+const optArticleSelector = '.post-title',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
 const titleClickHandler = function (event) {
   event.preventDefault();
   const clickedElement = this;
@@ -5,7 +9,6 @@ const titleClickHandler = function (event) {
 
   /* remove class 'active' from all article links */
   const activeLinks = document.querySelectorAll('.titles a.active');
-
   for (let activeLink of activeLinks) {
     activeLink.classList.remove('active');
   }
@@ -16,7 +19,6 @@ const titleClickHandler = function (event) {
 
   /* remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.posts article.active');
-
   for (let activeArticle of activeArticles) {
     activeArticle.classList.remove('active');
   }
@@ -37,20 +39,19 @@ const titleClickHandler = function (event) {
 const generateTitleLinks = function () {
   console.log('funkcja generateTitleLinks wykonala się');
   //szukanie kontenera na linki tytulow
-  const titleList = document.querySelector('.titles');
-
+  const titleList = document.querySelector(optTitleListSelector);
   //wyczyszczenie zawartosci kontenera
   titleList.innerHTML = '';
 
   //znalezienie wszystkich artykulow
   const articles = document.querySelectorAll('.posts .post');
-
   //dla kazdego artykulu
   for(let article of articles) {
     const articleId = article.getAttribute('id');
-    const articleTitle = article.querySelector('.post-title').innerText;
-    
-    const linkHTML = `<li><a href="#${articleId}"><span>${articleTitle}</span></a></li>`;
+    const articleTitle = article.querySelector(optArticleSelector).innerText;
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    console.log('wygenerowany link html: ', linkHTML);
+    //dodaje link do listy
     titleList.innerHTML += linkHTML;
   }
 
@@ -62,9 +63,6 @@ const generateTitleLinks = function () {
 };
 //wywoalnie generatetitlelinks po zaladowaniu strony
 generateTitleLinks();
-
-
-
 
 /*
 Nie wiem dlaczego cały blog po odpaleniu w wyszukiwarce przesuwa
